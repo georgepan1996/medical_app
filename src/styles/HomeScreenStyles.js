70aca0326e02d7570fc9dcf19a960b1baef3638a
George Panopoulos <georgios.panopoulos1996@gmail.com>
2022-03-16 23:46:00 +0200

    home screen bar-icons

--- src/styles/HomeScreenStyles.js
+++ src/styles/HomeScreenStyles.js
@@ -1,5 +1,41 @@
-import {StyleSheet} from "react-native-web";
+import { StyleSheet, Platform } from 'react-native';
 
+const primaryColor = '#5091ee'
 export default StyleSheet.create({
 
+    container: {
+        height: '100%'
+    },
+    header: {
+        backgroundColor: primaryColor,
+        height: '10%',
+        display: 'flex',
+        alignItems: 'flex-end',
+        paddingHorizontal: '1%',
+        paddingBottom: '2%',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    headerIconsLeft: {
+    },
+    headerIconsRight: {
+        display: 'flex',
+        flexDirection: 'row',
+    },
+    headerIcon: {
+        paddingHorizontal: '3%',
+    },
+    section: {
+        backgroundColor: '#FFF',
+        height: '80%'
+    },
+    footer: {
+        backgroundColor: primaryColor,
+        height: '10%',
+        display: 'flex',
+        alignItems: 'center',
+        paddingHorizontal: '1%',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    }
 })
